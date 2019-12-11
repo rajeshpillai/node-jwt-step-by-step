5d0a0dec7b4dec8e445f9e117b768c0ec3130007
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-12-11 12:07:22 +0530

    Created fakedb and register user

---
+++ db/fake-db.js
@@ -0,0 +1,7 @@
+exports.fakeDB = [
+  {
+    id: 1,
+    email: 'abc@test.com',
+    password: '$2a$10$nLCFR7WvrO2E2/lm36BEI.xlu6EegTVtwdTkXC59qcSuMM8UtzyI'
+  }
+]
--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const {verify} = require('jsonwebtoken');
 const {hash, compare} = require('bcryptjs');
+const {fakeDB} = require('../db/fake-db.js');
 
-// STEPS
-// 1. Register a user
-// 2. Login a user
-// 3. Logout a user
-// 4. Setup a protected route
-// 5. Get a new accesstoken with a refresh token
 
 const server = express();
 
@@ -34,3 +29,37 @@ server.use(express.urlencoded({
 server.listen(process.env.PORT, ()=> {
   console.log(`server listening on port ${process.env.PORT}`);
 });
+
+// STEPS
+// 1. Register a user
+server.post('/register', async(req, res) => {
+  const {email, password} = req.body;
+  try {
+
+      let user = fakeDB.find(user => user.email === email);
+      console.log(user);
+      if (user) throw new Error('User already exist');
+      const hashPassword = await hash(password,10);
+      fakeDB.push({
+        id: fakeDB.lenght,
+        email, 
+        password: hashPassword
+      });
+
+      console.log(fakeDB);
+
+      res.send({
+        message: "User successfully created."
+      })
+
+  } catch(err) {
+    res.send({
+      error: `${err.message}`
+    })
+  }
+})
+
+// 2. Login a user
+// 3. Logout a user
+// 4. Setup a protected route
+// 5. Get a new accesstoken with a refresh token
